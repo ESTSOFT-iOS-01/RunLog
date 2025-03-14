d1fce8133b953ea149d650c6735d09d34e1ceab4
shinseungjae <mok05247@naver.com>
2025-03-14 10:15:36 +0900

    [#8] Appconfig, DayLog, Section Entity 추가

---
+++ RunLog/RunLog/Sources/Domain/Entity/AppConfig.swift
@@ -0,0 +1,13 @@
+//
+//  AppConfig.swift
+//  RunLog
+//
+//  Created by 신승재 on 3/14/25.
+//
+
+import Foundation
+
+struct AppConfig {
+  var nickname: String
+  var unitDistance: Double
+}
---
+++ RunLog/RunLog/Sources/Domain/Entity/DayLog.swift
@@ -0,0 +1,23 @@
+//
+//  DayLog.swift
+//  RunLog
+//
+//  Created by 신승재 on 3/14/25.
+//
+
+import Foundation
+
+struct DayLog {
+  let date: Date
+  let locationName: String
+  let weather: Int
+  let temperature: Int
+  
+  var trackImage: Data
+  var title: String
+  var level: Int
+  var totalTime: TimeInterval
+  var totalDistance: Double
+  var totalSteps: Int
+  var sections: [Section]
+}
