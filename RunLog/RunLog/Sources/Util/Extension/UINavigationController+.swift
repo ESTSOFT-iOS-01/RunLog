dca2712521c37d0860570cf211d3cde827dc9d47
shinseungjae <mok05247@naver.com>
2025-03-14 16:08:12 +0900

    [#13] UINavigationController extension 추가

---
+++ RunLog/RunLog/Sources/Util/Extension/UINavigationController+.swift
@@ -0,0 +1,59 @@
+//
+//  UINavigationController+.swift
+//  RunLog
+//
+//  Created by 신승재 on 3/14/25.
+//
+
+import UIKit
+
+extension UINavigationController {
+    
+    /// 네비게이션 바의 스타일 설정
+    func setupAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .clear
+        appearance.shadowColor = .clear
+        appearance.titleTextAttributes = [
+            .foregroundColor: UIColor.Gray000,
+            .font: UIFont.RLHeadline1
+        ]
+        
+        navigationBar.standardAppearance = appearance
+        navigationBar.scrollEdgeAppearance = appearance
+        navigationBar.compactAppearance = appearance
+        navigationBar.tintColor = .LightGreen
+        
+        let backButton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationBar.topItem?.backBarButtonItem = backButton
+    }
+    
+    
+    /// 오른쪽 버튼 추가
+    func addRightButton(
+        title: String? = nil,
+        icon: String? = nil,
+        target: Any?,
+        action: Selector
+    ) {
+        let rightButton = UIButton(type: .system)
+        
+        if let title = title {
+            rightButton.setTitle(title, for: .normal)
+            rightButton.setTitleColor(.label, for: .normal)
+            rightButton.titleLabel?.font = .systemFont(
+                ofSize: 16,
+                weight: .medium
+            )
+        } else if let icon = icon {
+            rightButton.setImage(UIImage(systemName: icon), for: .normal)
+            rightButton.tintColor = .LightGreen
+        }
+        
+        rightButton.addTarget(target, action: action, for: .touchUpInside)
+        topViewController?.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            customView: rightButton
+        )
+    }
+}
