fbbae8249c0c1d6eeb47f64947ee942a8e660052
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-03-14 15:39:41 +0900

    [#14] 버튼 시스템 파일 생성

---
+++ RunLog/RunLog/Sources/Util/Constant/ButtonSystem.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonSystem.swift
+//  RunLog
+//
+//  Created by 김도연 on 3/14/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class ButtonSystem: UIButton {
+//    public init(
+//        title: String = "버튼",
+//        titleColor: UIColor = .white,
+//        
+//    )
+}
