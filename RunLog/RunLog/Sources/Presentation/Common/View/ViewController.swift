857060d6f3f40faf5d3aac60a6fdcdacb693385c
심근웅 <geunwoong.sim@gmail.com>
2025-03-14 13:18:55 +0900

    [#9] 디자인 시스템 세팅

--- RunLog/RunLog/Sources/Presentation/Common/View/ViewController.swift
+++ RunLog/RunLog/Sources/Presentation/Common/View/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 class ViewController: UIViewController {
-
+    var label = UILabel().then {
+        $0.attributedText = DesignSystemFont.Title.attributedString(
+            for: "Hello World!",
+            color: DesignSystemColor.Gray00.value
+        )
+        $0.backgroundColor = .blue
+    }
+    var closeButton = UIButton().then {
+        $0.setAttributedTitle(DesignSystemFont.Heading1.attributedString(
+            for: "닫기",
+            color: DesignSystemColor.Gray00.value
+        ), for: .normal)
+        $0.setImage(UIImage(systemName: DesignSystemIcon.closeButton.name), for: .normal)
+        $0.backgroundColor = UIColor.orange
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
         // Do any additional setup after loading the view.
+        [label, closeButton].forEach{ view.addSubview($0) }
+        
+        
+        view.addSubview(label)
+        label.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+        closeButton.snp.makeConstraints {
+            $0.leading.trailing.equalTo(label)
+            $0.top.equalTo(label.snp.bottom)
+        }
     }
-
 }
+
 
---
+++ RunLog/RunLog/Sources/Util/Extension/UIFont+Extension.swift
@@ -0,0 +1,31 @@
+//
+//  UIFont+Extension.swift
+//  RunLog
+//
+//  Created by 심근웅 on 3/14/25.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - 폰트 조절
+extension UIFont {
+    enum Pretendard: String {
+        case black = "Pretendard-Black"
+        case bold = "Pretendard-Bold"
+        case extraBold = "Pretendard-ExtraBold"
+        case extraLight = "Pretendard-ExtraLight"
+        case light = "Pretendard-Light"
+        case medium = "Pretendard-Medium"
+        case regular = "Pretendard-Regular"
+        case semiBold = "Pretendard-SemiBold"
+        case thin = "Pretendard-Thin"
+    }
+    static func pretendard(_ type: Pretendard, size: CGFloat) -> UIFont {
+        guard let font = UIFont(name: type.rawValue, size: size) else {
+            return UIFont.systemFont(ofSize: size)
+        }
+        return font
+    }
+}
+
