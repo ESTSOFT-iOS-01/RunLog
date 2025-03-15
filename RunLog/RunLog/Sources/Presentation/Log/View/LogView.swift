d51b41d7220baba244e96c05c21657f5e1fc7111
shinseungjae <mok05247@naver.com>
2025-03-15 11:08:16 +0900

    [#17] 기본 뷰, 뷰모델, 뷰컨 추가

---
+++ RunLog/RunLog/Sources/Presentation/Log/View/LogView.swift
@@ -0,0 +1,41 @@
+//
+//  LogView.swift
+//  RunLog
+//
+//  Created by 신승재 on 3/14/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class LogView: UIView {
+    
+    // MARK: - UI Components 선언
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup UI
+    private func setupUI() {
+        // UI 요소 추가
+    }
+    
+    // MARK: - Setup Layout
+    private func setupLayout() {
+        // 레이아웃 설정
+    }
+    
+    // MARK: - Configure
+    private func configure() {
+        // 뷰 설정
+    }
+}
