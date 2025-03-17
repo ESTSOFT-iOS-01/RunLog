d356d161c6e6a51ddd798789639e75186a5c448b
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-03-17 11:53:33 +0900

    [#28] 기록 시각화 단위 뷰 구현 완료

--- RunLog/RunLog/Sources/Presentation/Setting/View/CalUnitView.swift
+++ RunLog/RunLog/Sources/Presentation/Setting/View/CalUnitView.swift
@@ -10,16 +10,38 @@ import SnapKit
 import Then
 
 final class CalUnitView: UIView {
+    var unit : Double = 10.0
+    private let placeHolderString = "최대 100km까지 입력 가능합니다"
     
     // MARK: - UI Components 선언
-    private let placeHolderString = "최대 100km까지 입력 가능합니다"
     private lazy var exampleImageView = UIImageView().then {
+        $0.backgroundColor = .clear
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    lazy var despLabel = UILabel().then {
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
+        
+        let unitString = (unit*0.3).toString()
+        let fullText = "하루에 \(unitString)km 이동하면 이렇게 표시돼요"
+        
+        $0.attributedText = fullText.styledText(
+            highlightText: unitString,
+            baseFont: .RLBody1,
+            baseColor: .Gray000,
+            highlightFont: .RLBody1,
+            highlightColor: .LightGreen
+        )
+    }
+    
+    lazy var unitField = RLTextField(placeholder: placeHolderString)
+    
+    var exampleView = UIView().then {
         $0.backgroundColor = .Gray700
         $0.layer.cornerRadius = 12
         $0.clipsToBounds = true
-        $0.contentMode = .scaleAspectFit
     }
-    lazy var unitField = RLTextField(placeholder: placeHolderString)
     
     // MARK: - Init
     override init(frame: CGRect) {
@@ -36,19 +58,31 @@ final class CalUnitView: UIView {
     private func setupUI() {
         // UI 요소 추가
         backgroundColor = .Gray900
-        addSubviews(exampleImageView, unitField)
+        unitField.keyboardType = .decimalPad
+        exampleView.addSubviews(exampleImageView, despLabel)
+        addSubviews(exampleView, unitField)
     }
     
     // MARK: - Setup Layout
     private func setupLayout() {
         // 레이아웃 설정
         exampleImageView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.top.equalToSuperview().inset(32)
+        }
+        
+        despLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.bottom.equalToSuperview().inset(32)
+        }
+        
+        exampleView.snp.makeConstraints {
             $0.top.horizontalEdges.equalToSuperview()
             $0.height.equalTo(exampleImageView.snp.width).multipliedBy(256.0 / 393.0)
         }
         
         unitField.snp.makeConstraints {
-            $0.top.equalTo(exampleImageView.snp.bottom).offset(48)
+            $0.top.equalTo(exampleView.snp.bottom).offset(48)
             $0.horizontalEdges.equalToSuperview()
         }
     }
