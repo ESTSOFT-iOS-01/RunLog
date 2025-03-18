a5a1b1cc1adf0c9a2966e9fd5db0dc98cd88633a
shinseungjae <mok05247@naver.com>
2025-03-18 17:16:09 +0900

    [#41] DayLogMapping 추가

---
+++ RunLog/RunLog/Sources/Data/DTO/Mapping/DayLogMapping.swift
@@ -0,0 +1,26 @@
+//
+//  DayLogMapping.swift
+//  RunLog
+//
+//  Created by 신승재 on 3/18/25.
+//
+
+import Foundation
+
+extension DayLogDTO {
+//    func toEntity() -> DayLog {
+//        return DayLog(
+//            date: self.date!,
+//            locationName: self.locationName,
+//            weather: self.weather,
+//            temperature: self.temperature,
+//            trackImage: self.trackImage,
+//            title: self.title,
+//            level: self.level,
+//            totalTime: self.totalTime,
+//            totalDistance: self.totalDistance,
+//            totalSteps: self.totalSteps,
+//            sections: <#T##[Section]#>
+//        )
+//    }
+}
