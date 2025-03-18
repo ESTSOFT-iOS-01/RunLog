213b010cbf102047b66f884b619894e2741415cb
shinseungjae <mok05247@naver.com>
2025-03-18 19:03:30 +0900

    [#41] DataMapper 구현

---
+++ RunLog/RunLog/Sources/Data/DTO/DataMapper.swift
@@ -0,0 +1,103 @@
+//
+//  CoreDataMapper.swift
+//  RunLog
+//
+//  Created by 신승재 on 3/18/25.
+//
+
+import Foundation
+import CoreData
+
+
+// MARK: - ToDTO (Entity Model -> DTO Model)
+final class DataMapper {
+    
+    static func toDTO(_ entity: DayLog, context: NSManagedObjectContext) -> DayLogDTO {
+        let dto = DayLogDTO(context: context)
+        dto.date = entity.date
+        dto.locationName = entity.locationName
+        dto.weather = Int16(entity.weather)
+        dto.temperature = Int32(entity.temperature)
+        dto.trackImage = entity.trackImage
+        dto.title = entity.title
+        dto.level = Int16(entity.level)
+        dto.totalTime = entity.totalTime
+        dto.totalDistance = entity.totalDistance
+        dto.totalSteps = Int32(entity.totalSteps)
+        
+        let sectionDTOs = entity.sections.map { toDTO($0, context: context) }
+        dto.sections = NSSet(array: sectionDTOs)
+        
+        return dto
+    }
+    
+    static func toDTO(_ entity: Section, context: NSManagedObjectContext) -> SectionDTO {
+        let dto = SectionDTO(context: context)
+        dto.distance = entity.distance
+        dto.steps = Int32(entity.steps)
+        
+        let routeDTOs = entity.route.map { toDTO($0, context: context) }
+        dto.route = NSSet(array: routeDTOs)
+        
+        return dto
+    }
+    
+    static func toDTO(_ entity: Point, context: NSManagedObjectContext) -> PointDTO {
+        let dto = PointDTO(context: context)
+        dto.latitude = entity.latitude
+        dto.longitude = entity.longitude
+        dto.timestamp = entity.timestamp
+        return dto
+    }
+}
+
+
+// MARK: - ToEntity (DTO Model -> Entity Model)
+extension DataMapper {
+    static func toEntity(_ dto: DayLogDTO) -> DayLog? {
+        
+        guard let date = dto.date,
+              let locationName = dto.locationName,
+              let trackImage = dto.trackImage,
+              let title = dto.title
+        else { return nil }
+        
+        let sections = dto.sections as? Set<SectionDTO>
+        
+        return DayLog(
+            date: date,
+            locationName: locationName,
+            weather: Int(dto.weather),
+            temperature: Int(dto.temperature),
+            trackImage: trackImage,
+            title: title,
+            level: Int(dto.level),
+            totalTime: dto.totalTime,
+            totalDistance: dto.totalDistance,
+            totalSteps: Int(dto.totalSteps),
+            sections: sections?.compactMap { toEntity($0) } ?? []
+        )
+    }
+    
+    static func toEntity(_ dto: SectionDTO) -> Section? {
+        
+        let route = dto.route as? Set<PointDTO>
+        
+        return Section(
+            distance: dto.distance,
+            steps: Int(dto.steps),
+            route: route?.compactMap { toEntity($0) } ?? []
+        )
+    }
+    
+    static func toEntity(_ dto: PointDTO) -> Point? {
+        
+        guard let timestamp = dto.timestamp else { return nil }
+        
+        return Point(
+            latitude: dto.latitude,
+            longitude: dto.longitude,
+            timestamp: timestamp
+        )
+    }
+}
