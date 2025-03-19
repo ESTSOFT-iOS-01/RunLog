cb24da205cc6d6be34a429e9a64bf2c32d16b681
도민준 <132960058+aado97@users.noreply.github.com>
2025-03-19 09:23:05 +0900

    [#35] 씬 델리 수정

--- RunLog/RunLog/Sources/App/SceneDelegate.swift
+++ RunLog/RunLog/Sources/App/SceneDelegate.swift
@@ -12,20 +12,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//        guard let windowScene = (scene as? UIWindowScene) else { return }
-//        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-//        window?.windowScene = windowScene
-//        window?.rootViewController = MainTabBarController()
-//        window?.makeKeyAndVisible()
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        let detailViewModel = DetailLogViewModel()
-        let detailLogVC = DetailLogViewController(viewModel: detailViewModel)
-        
-        window?.rootViewController = UINavigationController(rootViewController: detailLogVC)
+        window?.rootViewController = MainTabBarController()
         window?.makeKeyAndVisible()
+        
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+//        window?.windowScene = windowScene
+//        let detailViewModel = DetailLogViewModel()
+//        let detailLogVC = DetailLogViewController(viewModel: detailViewModel)
+//        
+//        window?.rootViewController = UINavigationController(rootViewController: detailLogVC)
+//        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
