ed04a8e6870be8a440c1d669f201a524c001b009
shinseungjae <mok05247@naver.com>
2025-03-20 10:29:26 +0900

    [#51] 구현체 작성 및 CoreDataContainer 작성

--- RunLog/RunLog/Sources/Data/Impl/DayLogRepositoryImpl.swift
+++ RunLog/RunLog/Sources/Data/Impl/DayLogRepositoryImpl.swift
@@ -17,23 +17,94 @@ final class DayLogRepositoryImpl: DayLogRepository {
     }
     
     func createDayLog(_ dayLog: DayLog) async throws {
+        print("Impl: ", #function)
+        
         try await context.perform {
-            let dto = DataMapper.toDTO(dayLog, context: self.context)
-            self.context.insert(dto)
+            let data = DataMapper.toDTO(dayLog, context: self.context)
+            self.context.insert(data)
             try self.context.save()
         }
     }
-
+    
     func readDayLog(date: Date) async throws -> DayLog {
-        <#code#>
+        print("Impl: ", #function)
+        
+        return try await context.perform {
+            let fetchRequest: NSFetchRequest<DayLogDTO> = DayLogDTO.fetchRequest()
+            fetchRequest.predicate = NSPredicate(
+                format: "date == %@",
+                date as CVarArg
+            )
+            
+            guard let data = try self.context.fetch(fetchRequest).first else {
+                throw CoreDataError.modelNotFound
+            }
+            guard let model = DataMapper.toEntity(data) else {
+                throw CoreDataError.conversionError
+            }
+            
+            return model
+        }
+    }
+    
+    func readAllDayLogs() async throws -> [DayLog] {
+        print("Impl: ", #function)
+        
+        return try await context.perform {
+            let fetchRequest: NSFetchRequest<DayLogDTO> = DayLogDTO.fetchRequest()
+            fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+            
+            let datas = try self.context.fetch(fetchRequest)
+            
+            return datas.compactMap { DataMapper.toEntity($0) }
+        }
     }
 
     func updateDayLog(_ dayLog: DayLog) async throws {
-        <#code#>
+        print("Impl: ", #function)
+        
+        try await context.perform {
+            let fetchRequest: NSFetchRequest<DayLogDTO> = DayLogDTO.fetchRequest()
+            fetchRequest.predicate = NSPredicate(
+                format: "date == %@",
+                dayLog.date as CVarArg
+            )
+            
+            guard let data = try self.context.fetch(fetchRequest).first else {
+                throw CoreDataError.modelNotFound
+            }
+            
+            data.locationName = dayLog.locationName
+            data.weather = Int16(dayLog.weather)
+            data.temperature = Int32(dayLog.temperature)
+            data.trackImage = dayLog.trackImage
+            data.title = dayLog.title
+            data.level = Int16(dayLog.level)
+            data.totalTime = dayLog.totalTime
+            data.totalDistance = dayLog.totalDistance
+            data.totalSteps = Int32(dayLog.totalSteps)
+            data.sections = NSSet(array: dayLog.sections.map {
+                DataMapper.toDTO($0, context: self.context)
+            })
+        }
     }
 
     func deleteDayLog(date: Date) async throws {
-        <#code#>
+        print("Impl: ", #function)
+        
+        try await context.perform {
+            let fetchRequest: NSFetchRequest<DayLogDTO> = DayLogDTO.fetchRequest()
+            fetchRequest.predicate = NSPredicate(
+                format: "date == %@",
+                date as CVarArg
+            )
+            
+            guard let data = try self.context.fetch(fetchRequest).first else {
+                throw CoreDataError.modelNotFound
+            }
+            
+            self.context.delete(data)
+            try self.context.save()
+        }
     }
-    
 }
