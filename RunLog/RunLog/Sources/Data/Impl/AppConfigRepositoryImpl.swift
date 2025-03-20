bd03c07ae7bf750c36337c9620fe20fb40d26f6f
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-03-20 22:37:11 +0900

    [#60] AppconfigRepoImpl thread-safe하게 수정

--- RunLog/RunLog/Sources/Data/Impl/AppConfigRepositoryImpl.swift
+++ RunLog/RunLog/Sources/Data/Impl/AppConfigRepositoryImpl.swift
@@ -16,66 +16,104 @@ final class AppConfigRepositoryImpl: AppConfigRepository {
     }
     
     func createAppConfig(_ config: AppConfig) async throws {
-        let _ = DataMapper.toDTO(config, context: context)
-        do {
-            try context.save()
-        } catch {
-            throw AppConfigError.saveFailed
-        }
+        print("Impl: ", #function)
+        
+            try await context.perform { [weak self] in
+                guard let self = self else {
+                    throw AppConfigError.notFound
+                }
+                
+                let data = DataMapper.toDTO(config, context: self.context)
+                
+                do {
+                    try self.context.save()
+                } catch {
+                    throw AppConfigError.saveFailed
+                }
+            }
     }
     
     func readAppConfig() async throws -> AppConfig {
-        let fetchRequest: NSFetchRequest<AppConfigDTO> = AppConfigDTO.fetchRequest()
-        let results = try context.fetch(fetchRequest)
-
-        guard let entity = results.first else {
-            throw AppConfigError.notFound
+        print("Impl: ", #function)
+        
+        return try await context.perform { [weak self] in
+            guard let self = self else {
+                // 객체가 Nil
+                throw AppConfigError.notFound
+            }
+            
+            let fetchRequest: NSFetchRequest<AppConfigDTO> = AppConfigDTO.fetchRequest()
+            
+            guard let data = try self.context.fetch(fetchRequest).first else {
+                throw AppConfigError.notFound
+            }
+            
+            guard let config = DataMapper.toEntity(data) else {
+                throw AppConfigError.dataConversionFailed
+            }
+            
+            return config
         }
-
-        guard let config = DataMapper.toEntity(entity) else {
-            throw AppConfigError.dataConversionFailed
-        }
-
-        return config
+        
     }
     
     func updateAppConfig(_ config: AppConfig) async throws {
-        let fetchRequest: NSFetchRequest<AppConfigDTO> = AppConfigDTO.fetchRequest()
-        let results = try context.fetch(fetchRequest)
-
-        guard let entity = results.first else {
-            throw AppConfigError.notFound
-        }
+        print("Impl: ", #function)
         
-        entity.nickname = config.nickname
-        entity.streakDays = Int32(config.streakDays)
-        entity.totalDays = Int32(config.totalDays)
-        entity.totalDistance = config.totalDistance
-        entity.unitDistance = config.unitDistance
-        
-        do {
-            try context.save()
-        } catch {
-            throw AppConfigError.saveFailed
+        return try await context.perform { [weak self] in
+            guard let self = self else {
+                // 객체가 Nil
+                throw AppConfigError.notFound
+            }
+            
+            let fetchRequest: NSFetchRequest<AppConfigDTO> = AppConfigDTO.fetchRequest()
+            
+            guard let entity = try self.context.fetch(fetchRequest).first else {
+                throw AppConfigError.notFound
+            }
+            
+            ///  들어온 데이터로 프로퍼티 업데이트
+            entity.nickname = config.nickname
+            entity.streakDays = Int32(config.streakDays)
+            entity.totalDays = Int32(config.totalDays)
+            entity.totalDistance = config.totalDistance
+            entity.unitDistance = config.unitDistance
+            
+            do {
+                try self.context.save()
+            } catch {
+                throw AppConfigError.saveFailed
+            }
+            
         }
     }
     
     func deleteAppConfig() async throws {
-        let fetchRequest: NSFetchRequest<AppConfigDTO> = AppConfigDTO.fetchRequest()
-        let results = try context.fetch(fetchRequest)
-
-        guard !results.isEmpty else {
-            throw AppConfigError.notFound
-        }
-
-        for entity in results {
-            context.delete(entity)
-        }
+        print("Impl: ", #function)
         
-        do {
-            try context.save()
-        } catch {
-            throw AppConfigError.deleteFailed
+        return try await context.perform { [weak self] in
+            guard let self = self else {
+                // 객체가 Nil
+                throw AppConfigError.notFound
+            }
+            
+            let fetchRequest: NSFetchRequest<AppConfigDTO> = AppConfigDTO.fetchRequest()
+            let results = try self.context.fetch(fetchRequest)
+            
+            // 빈 배열(데이터 하나도 없는 경우)
+            guard !results.isEmpty else {
+                throw AppConfigError.notFound
+            }
+            
+            for entity in results {
+                context.delete(entity)
+            }
+            
+            do {
+                try context.save()
+            } catch {
+                throw AppConfigError.deleteFailed
+            }
         }
     }
     
