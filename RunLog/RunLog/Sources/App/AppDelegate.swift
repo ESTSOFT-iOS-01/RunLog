ef26287e00b3969b1db08e58c565e99cd81f9cb5
shinseungjae <mok05247@naver.com>
2025-03-20 23:06:19 +0900

    [#65] DIContainer 추가

--- RunLog/RunLog/Sources/App/AppDelegate.swift
+++ RunLog/RunLog/Sources/App/AppDelegate.swift
@@ -10,10 +10,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        registerDependencies()
+        
         return true
     }
 
@@ -31,6 +31,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    private func registerDependencies() {
+        let coreDataContainer = CoreDataContainer()
+        let appConfigRepository = AppConfigRepositoryImpl(
+            context: coreDataContainer.context
+        )
+        let dayLogRepository = DayLogRepositoryImpl(
+            context: coreDataContainer.context
+        )
+        
+        DIContainer.shared.register(
+            AppConfigUsecaseImpl(appConfigRepository: appConfigRepository)
+        )
+        DIContainer.shared.register(
+            DayLogUseCaseImpl(dayLogRepository: dayLogRepository)
+        )
+        
+    }
 }
 
---
+++ RunLog/RunLog/Sources/App/DIContainer.swift
@@ -0,0 +1,33 @@
+//
+//  DIContainer.swift
+//  RunLog
+//
+//  Created by 신승재 on 3/20/25.
+//
+
+import Foundation
+
+class DIContainer {
+    
+    static let shared = DIContainer()
+    
+    private var dependencies = [String: Any]()
+    
+    private init() {}
+
+    func register<T>(_ dependency: T) {
+        let key = String(describing: type(of: T.self))
+        dependencies[key] = dependency
+    }
+    
+    func resolve<T>() -> T {
+        let key = String(describing: type(of: T.self))
+        let dependency = dependencies[key]
+        
+        precondition(
+            dependency != nil, "\(key)는 register되지 않았어어요. resolve 부르기전에 register 해주세요"
+        )
+        
+        return dependency as! T
+    }
+}
