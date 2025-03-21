756e906660d32481319f3f10292a4a8178997125
심근웅 <geunwoong.sim@gmail.com>
2025-03-21 13:16:54 +0900

    [#47] Bundle 익스텐션 수정

--- RunLog/RunLog/Sources/Util/Extension/Bundle+.swift
+++ RunLog/RunLog/Sources/Util/Extension/Bundle+.swift
@@ -8,6 +8,9 @@
 import Foundation
 extension Bundle {
     var weatherKey: String? {
-        return infoDictionary?["API_KEY"] as? String
+        guard let key = object(forInfoDictionaryKey: "API_KEY") as? String else {
+            fatalError("❌ API_KEY가 Info.plist에서 설정되지 않았습니다.")
+        }
+        return key
     }
 }
