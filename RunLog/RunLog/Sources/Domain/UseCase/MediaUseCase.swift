a174c34ba1a6de67633a72512a96dd98b40afaf2
shinseungjae <mok05247@naver.com>
2025-03-21 09:17:17 +0900

    [#65] MediaUseCase&Impl 추가

---
+++ RunLog/RunLog/Sources/Domain/UseCase/MediaUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  MediaUseCase.swift
+//  RunLog
+//
+//  Created by 신승재 on 3/21/25.
+//
+
+import Foundation
---
+++ RunLog/RunLog/Sources/Domain/UseCaseImpl/MediaUseCaseImpl.swift
@@ -0,0 +1,8 @@
+//
+//  MediaUseCaseImpl.swift
+//  RunLog
+//
+//  Created by 신승재 on 3/21/25.
+//
+
+import Foundation
