cb5b30b5dbee07a13be4950d1d296ef294e225fa
도민준 <132960058+aado97@users.noreply.github.com>
2025-03-24 16:06:01 +0900

    [#105] 난이도 수정

--- RunLog/RunLog/Sources/Util/Extension/Int+.swift
+++ RunLog/RunLog/Sources/Util/Extension/Int+.swift
@@ -27,11 +27,11 @@ extension Int {
         
         func toLevelDescription() -> String {
             switch self {
-            case 1: return "매우 쉬움"
-            case 2: return "쉬움"
-            case 3: return "보통"
-            case 4: return "어려움"
-            case 5: return "매우 어려움"
+            case 0: return "매우 쉬움"
+            case 1: return "쉬움"
+            case 2: return "보통"
+            case 3: return "어려움"
+            case 4: return "매우 어려움"
             default: return "알 수 없음"
             }
         }
