233a2fcc9fe64643a88975f3f93abfd5c9871dd6
도민준 <132960058+aado97@users.noreply.github.com>
2025-03-24 15:30:26 +0900

    [#105] 셀 터치시 줌처리 , 위치표기 수정

--- RunLog/RunLog/Sources/Presentation/DetailLog/Model/DisplayDayLog.swift
+++ RunLog/RunLog/Sources/Presentation/DetailLog/Model/DisplayDayLog.swift
@@ -25,7 +25,30 @@ struct DisplayDayLog {
 extension DisplayDayLog {
     init(from dayLog: DayLog) {
         self.date = dayLog.date
-        self.locationName = dayLog.locationName
+        let splitted = dayLog.locationName.split(separator: " ").map { String($0) }
+        
+        if splitted.isEmpty {
+            // 토큰이 전혀 없는 경우
+            self.locationName = ""
+        }
+        else if splitted.count == 1 {
+            // 예) "서울특별시", "대구광역시"
+            self.locationName = splitted[0]
+        }
+        else {
+            // splitted.count >= 2
+            let first = splitted[0]
+            let second = splitted[1]
+            
+            // 두 번째 토큰이 "시"로 끝나면(경산시, 수원시 등) => 2개 토큰 유지
+            if second.hasSuffix("시") {
+                self.locationName = first + " " + second
+            } else {
+                // 그 외(구, 동 등)는 필요 없으니 첫 토큰만
+                self.locationName = first
+            }
+        }
+        
         self.weather = Constants.WeatherCondition.from(dayLog.weather).description
         self.temperature = dayLog.temperature
         self.title = dayLog.title
