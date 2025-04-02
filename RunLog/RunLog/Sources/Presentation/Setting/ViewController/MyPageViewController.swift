a95833fd8c2e5c5d2735a574a57b55f91c18eaeb
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-04-02 15:46:35 +0900

    [#119] 세팅 뷰컨트롤러 코드 정리 및 문서화

--- RunLog/RunLog/Sources/Presentation/Setting/ViewController/MyPageViewController.swift
+++ RunLog/RunLog/Sources/Presentation/Setting/ViewController/MyPageViewController.swift
@@ -10,6 +10,7 @@ import SnapKit
 import Then
 import Combine
 
+/// 마이페이지 화면을 구성하는 ViewController
 final class MyPageViewController: UIViewController {
     
     // MARK: - Properties
@@ -33,17 +34,17 @@ final class MyPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupUI()
-        setupNavigationBar()
-        setupTableView()
+        setupUI()                 // 화면 구성
+        setupNavigationBar()     // 네비게이션 바 설정
+        setupTableView()         // 테이블뷰 바인딩
         
-        viewModel.bind()
-        bindViewModel()
+        viewModel.bind()         // ViewModel의 입력 수신 준비
+        bindViewModel()          // ViewModel의 출력 바인딩
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        viewModel.input.send(.loadData)
+        viewModel.input.send(.loadData) // 데이터 새로 불러오기
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -52,7 +53,6 @@ final class MyPageViewController: UIViewController {
     
     // MARK: - Setup UI
     private func setupUI() {
-        // UI 요소 추가
         view.backgroundColor = .Gray900
         view.addSubview(mypageView)
         
@@ -64,7 +64,6 @@ final class MyPageViewController: UIViewController {
     
     // MARK: - Setup Navigation Bar
     private func setupNavigationBar() {
-        // 네비게이션바 디테일 설정
         self.navigationController?.setupAppearance()
         self.navigationController?.setupLogoTitle()
     }
@@ -74,8 +73,9 @@ final class MyPageViewController: UIViewController {
         mypageView.tableView.dataSource = self
     }
 
-    // MARK: - Bind ViewModel
+    // MARK: - ViewModel Output Binding
     private func bindViewModel() {
+        // 로딩 상태 변경
         viewModel.output.stopLoading
             .receive(on: DispatchQueue.main)
             .sink { [weak self] off in
@@ -83,6 +83,7 @@ final class MyPageViewController: UIViewController {
             }
             .store(in: &cancellables)
         
+        // 프로필 데이터 업데이트
         viewModel.output.profileDataUpdated
             .receive(on: DispatchQueue.main)
             .sink { [weak self] config in
@@ -90,6 +91,7 @@ final class MyPageViewController: UIViewController {
             }
             .store(in: &cancellables)
         
+        // 화면 전환 처리
         viewModel.output.navigateToViewController
             .receive(on: DispatchQueue.main)
             .sink { [weak self] viewController in
@@ -101,8 +103,9 @@ final class MyPageViewController: UIViewController {
     }
 }
 
-
+// MARK: - UITableViewDelegate
 extension MyPageViewController: UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "설정"
     }
@@ -110,25 +113,32 @@ extension MyPageViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         guard let header = view as? UITableViewHeaderFooterView else { return }
         
+        // 섹션 헤더 스타일 커스터마이징
         header.textLabel?.frame.origin.x = 4
         header.textLabel?.textAlignment = .left
-        header.textLabel?.attributedText = .RLAttributedString(text: header.textLabel?.text ?? "설정",
-                                                               font: .Label1,
-                                                               color: .Gray100)
+        header.textLabel?.attributedText = .RLAttributedString(
+            text: header.textLabel?.text ?? "설정",
+            font: .Label1,
+            color: .Gray100
+        )
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 메뉴 항목 선택 시 ViewModel에 전달
         tableView.deselectRow(at: indexPath, animated: true)
         viewModel.input.send(.menuItemSelected(indexPath.row))
     }
 }
 
+// MARK: - UITableViewDataSource
 extension MyPageViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return SettingMenuType.allCases.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 메뉴 셀 구성
         guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingMenuCell.identifier, for: indexPath) as? SettingMenuCell else {
             return UITableViewCell()
         }
@@ -136,5 +146,4 @@ extension MyPageViewController: UITableViewDataSource {
         cell.configure(title: SettingMenuType.allCases[indexPath.row].title)
         return cell
     }
-    
 }
