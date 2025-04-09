936c5d6d7616f11363e6b9dc414cc8a25e41de8b
심근웅 <geunwoong.sim@gmail.com>
2025-04-09 13:42:28 +0900

    [#127] Util 파트 주석 추가

--- RunLog/RunLog/Sources/Util/Extension/CLPlacemark+.swift
+++ RunLog/RunLog/Sources/Util/Extension/CLPlacemark+.swift
@@ -10,29 +10,56 @@ import MapKit
 
 // MARK: - 위치 정보 -> 한글
 extension CLPlacemark {
+    
+    /// CLPlacemark 정보를 "도(광역시) 시(군,구) 동(읍,면)"의 형태로 변경하여 반환합니다.
     func placemarksToString() -> String {
+        
         var state: String = "" // 도, 광역시
         var city: String = "" // 시, 군, 구
         var district: String = "" // 동, 읍, 면
+        
         if let subLocal = self.subLocality, subLocal.hasSuffix("동") {
             district = subLocal
         }
+        
         guard let description = self.description
             .split(separator: ",")
             .filter({ $0.contains("대한민국") })
             .first
-        else { return Constants.LocationMessage.random.message }
+        else {
+            return Constants.LocationMessage.random.message
+        }
         
         let components = description.split(separator: " ").map { String($0) }
+        
         for component in components {
-            if state == "" && (component.hasSuffix("특별시") || component.hasSuffix("광역시") || component.hasSuffix("도")) {
+            
+            if state == "" && (
+                component.hasSuffix("특별시") ||
+                component.hasSuffix("광역시") ||
+                component.hasSuffix("도")
+            ) {
                 state = component
-            }else if city == "" && (component.hasSuffix("시") || component.hasSuffix("군") || component.hasSuffix("구")) {
+                
+            } else if city == "" && (
+                component.hasSuffix("시") ||
+                component.hasSuffix("군") ||
+                component.hasSuffix("구")
+            ) {
                 city = component
-            }else if district == "" && (component.hasSuffix("동") || component.hasSuffix("읍") || component.hasSuffix("면") || component.hasSuffix("로")) {
+                
+            } else if district == "" && (
+                component.hasSuffix("동") ||
+                component.hasSuffix("읍") ||
+                component.hasSuffix("면") ||
+                component.hasSuffix("로")
+            ) {
                 district = component
+                
             }
         }
+        
+        // district는 있는 경우에만 포함
         return district.isEmpty ? "\(state) \(city)" : "\(state) \(city) \(district)"
     }
 }
--- RunLog/RunLog/Sources/Util/Extension/MKMapView+.swift
+++ RunLog/RunLog/Sources/Util/Extension/MKMapView+.swift
@@ -9,25 +9,35 @@ import Foundation
 import MapKit
 
 extension MKMapView {
-    // MARK: - 카메라 위치 변경
+    
+    /// 위치와 보여질 거리, 기존에 지정된 카메라위치에 따라 카메라 위치를 지정합니다.
+    /// - Parameters:
+    ///   - location: 표현하고자하는 위치
+    ///   - regionRadius: 최대로 보여질 지도 거리
+    ///   - region: 기존 카메라 상태
     func centerToLocation(
         _ location: CLLocation,
         regionRadius: CLLocationDistance = 150, // 주변 거리(미터)
         region: MKCoordinateRegion? = nil
     ) {
-        if let region = region {
+        
+        if let region = region { // 기존의 줌이 존재하면 해당 줌을 유지
+            
             let updatedRegion = MKCoordinateRegion(
                 center: location.coordinate,
                 span: region.span // 기존 줌(span) 유지
             )
             setRegion(updatedRegion, animated: true)
-        } else {
+            
+        } else { // 기존 줌이 존재하지 않다면 거리에 대비해 카메라 줌을 조정
+            
             let coordinateRegion = MKCoordinateRegion(
                 center: location.coordinate,
                 latitudinalMeters: regionRadius,
                 longitudinalMeters: regionRadius
             )
             setRegion(coordinateRegion, animated: true)
+            
         }
         
         let currentCamera = self.camera
